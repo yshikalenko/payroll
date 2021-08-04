43c93d4c4c899a7c1796762b92a9352c53794e4c
yshikalenko <shikalenko@gmail.com>
2021-08-04 15:53:27 +0600

    Make runnable on tomcat

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/")
 class EmployeeController {
 
   private final EmployeeRepository repository;
